be06f239256747c4c7f28bd596f0057beb2b081c
FarmanOthman <farmaanothmaan@gmail.com>
2025-05-03 10:01:53 +0300

    Add blog post and comment management routes

--- Challenge-3/blog-app/routes/web.php
+++ Challenge-3/blog-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +14,27 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
+});
+
+// Blog post routes
+Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
+Route::get('/blog/{slug}', [PostController::class, 'show'])->name('posts.show');
+
+// Authenticated blog routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Post management
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    
+    // Comment management
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+    Route::patch('/comments/{comment}/toggle-approval', [CommentController::class, 'toggleApproval'])
+        ->name('comments.toggle-approval');
 });
 
 Route::get('/dashboard', function () {
