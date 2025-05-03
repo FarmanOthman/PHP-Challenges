862781402c73e16c1f30d60b3e06fb737b009d3b
FarmanOthman <farmaanothmaan@gmail.com>
2025-05-03 10:15:47 +0300

    Refactor dashboard route to use DashboardController for better structure

--- Challenge-3/blog-app/routes/web.php
+++ Challenge-3/blog-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
@@ -37,11 +38,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
         ->name('comments.toggle-approval');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
