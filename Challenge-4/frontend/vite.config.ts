6d8b18f5fe19b1273efe0ebd2a1a1d3c4cd089bc
FarmanOthman <farmaanothmaan@gmail.com>
2025-06-02 21:51:55 +0300

    feat: add server configuration to Vite for local IP listening and default port

--- Challenge-4/frontend/vite.config.ts
+++ Challenge-4/frontend/vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from "vite-tsconfig-paths"
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',  // Listen on all local IPs
+    port: 5173,       // Default Vite port
+  }
 })
